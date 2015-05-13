391ccb0312a43aea8bc008061b0e74418a173a3c
Matt Clarkson <Matt.clarkson@comparethemarket.com>
2015-05-13 20:25:15 +0100

    Added controllers, modules and broken service lol

---
+++ app/controllers/talkController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("lightningVoting")
+        .controller("TalkController", ["talkService", talkController]);
+
+    function talkController(talkService) {
+        var model = this;
+        model.talks = talkService.getTalks();
+    };
+
+}());
---
+++ app/services/talkService.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    angular.module("lightningTalk").factory("talkService", [getTalks()]);
+
+    function getTalks() {
+        return [
+        {
+            title: "blah",
+            speaker: "bob",
+            tags: ["mvc", ".NET"]
+        },
+        {
+            title: "blah2",
+            speaker: "bob2",
+            tags: ["mvc2", ".NET2"]
+        }];
+    }  
+}());
